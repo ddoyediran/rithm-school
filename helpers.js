4b7e11178ea502e8fe560ceb1ad7c1e7b4796ce8
ddoyediran <ddoyediran@gmail.com>
2022-05-29 23:27:53 +0200

    first commit

---
+++ helpers.js
@@ -0,0 +1,7 @@
+module.exports = {
+  // module.exports object to export and object
+  name: "Elise",
+  sayHi() {
+    console.log(`Hi ${this.name}!`);
+  },
+};
---
+++ main.js
@@ -0,0 +1,9 @@
+// importing modules
+const helpers = require("./helpers"); // require to import a file
+const moreHelpers = require("./moreHelpers");
+
+helpers.sayHi();
+
+console.log(moreHelpers.firstName);
+moreHelpers.sayHello();
+moreHelpers.sayGoodbye();
