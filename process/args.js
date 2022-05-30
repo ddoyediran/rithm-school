7bc6a407cdb697b683ca628c08556abe25c88bc0
ddoyediran <ddoyediran@gmail.com>
2022-05-30 14:29:08 +0200

    more commit

---
+++ process/args.js
@@ -0,0 +1,7 @@
+/**
+ * Process can be used for a lot of things including:
+ * 1. Accepting command line arguments (argv) and listening for specific events (on)
+ * 2. Process object can also be used to access environment variables using process.env
+ */
+
+console.log(process.argv);
